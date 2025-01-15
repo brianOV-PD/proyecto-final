d44d853fe37ed40a44a7a06cc1ca02f6a412e572
brianOV-PD <villcaocampobrian@gmail.com>
2025-01-15 01:57:06 -0300

    Add files via upload

---
+++ java/java.js
@@ -0,0 +1,10 @@
+function cambiar() {
+  document.body.style.backgroundColor= "#505050";
+  document.body.style.color= "#FFFFFF";
+  document.nav.style.color= "#FFFFFF";
+}
+
+function cambiar2() {
+  document.body.style.backgroundColor= "#FFFFFF";
+  document.body.style.color= "#000000";
+}
